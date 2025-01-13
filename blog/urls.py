5f23236c538913fb0fc527d117e9015b4c1683bc
rahulp <rahulp@qburst.com>
2025-01-13 11:33:46 +0530

    Add urls for my blogs.

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.start_page, name="starting_page"),
+    path("posts", views.all_posts, name="posts_page"),
+    path("posts/<slug:post_name>", views.individual_post, name="posts_detail_page")
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def start_page(req):
+    return HttpResponse("I am starting page")
+
+def all_posts(req):
+    return HttpResponse("All Posts")
+
+def individual_post(req, post_name):
+    return HttpResponse(f"Post name is: {post_name}")
