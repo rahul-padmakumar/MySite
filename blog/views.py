8d7a47fa0fc8fbce08a502a2f988275bb8e9f44f
rahulp <rahulp@qburst.com>
2025-01-13 14:35:12 +0530

    Add basic design for post listing screen.

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def start_page(req):
     return render(req, "blog/start_page.html")
 
 def all_posts(req):
-    return HttpResponse("All Posts")
+    return render(req, "blog/all_posts.html")
 
 def individual_post(req, post_name):
     return HttpResponse(f"Post name is: {post_name}")
