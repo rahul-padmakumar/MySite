c0ec65e8d1b5ba9ab87a6984d2801e91268d37df
rahulp <rahulp@qburst.com>
2025-01-27 11:56:34 +0530

    Add models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Tag, Author
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=500)
+    content = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+    slug = models.SlugField()
+    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title}, {self.author.first_name}"
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
