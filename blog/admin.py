1f42eaa24266cec711b99f9e89de1062ac1495cc
rahulp <rahulp@qburst.com>
2025-01-28 09:39:38 +0530

    Add db and display post from Db.

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from .models import Post, Tag, Author
 
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ('slug',)
+    prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author","date", "tag")
+    list_display = ("title", "date", "author")
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Tag(models.Model):
@@ -11,7 +12,7 @@ class Tag(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email_address = models.CharField(max_length=100)
+    email_address = models.EmailField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -20,11 +21,12 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=500)
-    content = models.CharField(max_length=1000)
+    content = models.TextField(validators=[MinLengthValidator(10)])
     image = models.CharField(max_length=1000)
-    slug = models.SlugField()
-    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name="posts")
     tag = models.ManyToManyField(Tag)
+    date = models.DateField(auto_now=True)
 
     def __str__(self):
         return f"{self.title}, {self.author.first_name}"
