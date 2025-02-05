22d3c4627c71a5aa6de4e75ecdf92d2f5b0e50bc
rahulp <rahulp5875@gmail.com>
2025-02-05 13:27:24 +0530

    Adding option for posting and viewing comments in detail screen.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from datetime import date
 from .models import Post
 from django.views.generic import ListView, DeleteView
+from django.views import View
 from .forms import CommentForm
+from django.urls import reverse
 
 # Create your views here.
 
@@ -23,15 +25,32 @@ class AllPostView(ListView):
     context_object_name = "all_posts"
     ordering = "-date"
 
-class POSTDetailView(DeleteView):
-    template_name = "blog/post_details.html"
-    model = Post
-    context_object_name="identified_post"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tags"] = self.get_object().tag.all()
-        context["form"] = CommentForm()
-        return context
+class POSTDetailView(View):
+
+    def get_context(self, post, form):
+        return {
+            "identified_post": post,
+            "tags": post.tag.all(),
+            "form": form,
+            "comments": post.comments.all().order_by("-id")
+        }
+
+    def get(self, req, slug):
+        context = self.get_context(
+            Post.objects.get(slug = slug),
+            CommentForm()
+        )
+        return render(req, "blog/post_details.html", context)
+    
+    def post(self, req, slug):
+        form = CommentForm(req.POST)
+        post = Post.objects.get(slug = slug)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("posts_detail_page", args=[slug]))
+        return render(req, "blog/post_details.html", self.get_context(post, form))
     
 
 def get_date(post):
