44590b713acc6b6333d327a8fd52b9575534d71c
rahulp <rahulp5875@gmail.com>
2025-02-05 13:29:55 +0530

    Register cooment model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Tag, Author
+from .models import Post, Tag, Author, CommentModel
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
     list_filter = ("author","date", "tag")
     list_display = ("title", "date", "author")
 
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "comment_text",)
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(CommentModel, CommentModelAdmin)
