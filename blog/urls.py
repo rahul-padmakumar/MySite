291d762aa762ec4001b6066399898d181fba3ba3
rahulp <rahulp5875@gmail.com>
2025-02-05 12:08:45 +0530

    Use ListView for starting page.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.start_page, name="starting_page"),
+    path("", views.StartPageView.as_view(), name="starting_page"),
     path("posts", views.all_posts, name="posts_page"),
     path("posts/<slug:slug>", views.individual_post, name="posts_detail_page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
+class StartPageView(ListView):
+    template_name = "blog/start_page.html"
+    model = Post
+    context_object_name = "latest_posts"
+    ordering = "-date"
+    def get_queryset(self):
+        query = super().get_queryset()
+        data = query[:3]
+        return data
+
 def get_date(post):
     return post['date']
-
-
-def start_page(req):
-    posts = Post.objects.all().order_by("-date")[:3]
-    return render(req, "blog/start_page.html", {"latest_posts":posts})
 
 def all_posts(req):
     posts = Post.objects.all().order_by("-date")
