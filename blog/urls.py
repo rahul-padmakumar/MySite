d8ef95f588e28130caeda7bfb20d30ce372580fa
rahulp <rahulp5875@gmail.com>
2025-02-06 14:40:59 +0530

    Complete read later feature.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.StartPageView.as_view(), name="starting_page"),
     path("posts", views.AllPostView.as_view(), name="posts_page"),
-    path("posts/<slug:slug>", views.POSTDetailView.as_view(), name="posts_detail_page")
+    path("posts/<slug:slug>", views.POSTDetailView.as_view(), name="posts_detail_page"),
+    path("read_later", views.ReadLaterView.as_view(), name="read_later")
 ]
--- blog/views.py
+++ blog/views.py
@@ -27,18 +27,29 @@ class AllPostView(ListView):
 
 class POSTDetailView(View):
 
-    def get_context(self, post, form):
+    def is_saved_posts(self, req, post_id):
+        saved_post_ids = req.session.get("saved_posts")
+        print(type(post_id))
+        print(type(saved_post_ids))
+        if saved_post_ids is not None:
+            return post_id in saved_post_ids
+        else:
+            return False
+
+    def get_context(self, post, form, req):
         return {
             "identified_post": post,
             "tags": post.tag.all(),
             "form": form,
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "is_saved_post": self.is_saved_posts(req, post.id)
         }
 
     def get(self, req, slug):
         context = self.get_context(
             Post.objects.get(slug = slug),
-            CommentForm()
+            CommentForm(),
+            req=req
         )
         return render(req, "blog/post_details.html", context)
     
@@ -50,7 +61,38 @@ class POSTDetailView(View):
             comment.post = post
             comment.save()
             return HttpResponseRedirect(reverse("posts_detail_page", args=[slug]))
-        return render(req, "blog/post_details.html", self.get_context(post, form))
+        return render(req, "blog/post_details.html", self.get_context(post, form, req))
+    
+
+class ReadLaterView(View):
+    def get(self, req):
+        saved_post_ids = req.session.get("saved_posts")
+        if saved_post_ids is None or len(saved_post_ids) == 0:
+            saved_posts = []
+            has_saved_post = False
+        else:
+            saved_posts = Post.objects.filter(id__in=saved_post_ids)
+            has_saved_post = True
+        print(saved_posts)
+        return render(req, "blog/read_later.html", {"saved_posts":saved_posts, "has_saved_posts": has_saved_post})        
+
+    def post(self, req):
+        post_id = int(req.POST["post_id"])
+        saved_post_ids = req.session.get("saved_posts")
+
+        if saved_post_ids is None:
+            saved_post_ids = []
+
+        for id in saved_post_ids:
+            print(type(id))
+
+        if post_id in saved_post_ids:
+            saved_post_ids.remove(post_id)
+        else:
+            saved_post_ids.append(post_id)
+
+        req.session["saved_posts"] = saved_post_ids
+        return HttpResponseRedirect("/")
     
 
 def get_date(post):
